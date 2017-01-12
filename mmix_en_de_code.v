a8159a8a563e5adaa7ee48c685c1058aa43dbb9e
romisfrag <romisfrag@gmail.com>
2017-01-12 17:09:27 +0100

    trying some stuffs

--- mmix_en_de_code.v
+++ mmix_en_de_code.v
@@ -2,7 +2,11 @@ Require Export Bool Sumbool.
 Require Vector.
 Export Vector.VectorNotations.
 Require Import Minus.
-
+Require Import Arith.
+Require Import Ascii.
+Require String.
+Open Scope string_scope.
+Open Scope list_scope.
 Module mmix_en_de_code.
 
   (* datatypes for the instructions *)
@@ -10,10 +14,20 @@ Module mmix_en_de_code.
   Inductive etiquette : Type :=
 | ADD : etiquette
 | AND : etiquette.
+
+  (* there is 256 register and 32 special register *)
+  (* i only give two special register to test but i will not give the exhaustive list (at least for the moment) *)
+  Inductive special_register : Type :=
+  | rB : special_register
+  | rD : special_register.
+  
+  Inductive registre : Type :=
+  | general_reg : nat -> registre
+  | special_reg : special_register -> registre.
   
   Inductive operande : Type :=
   | immediate : nat -> operande
-  | reg : nat -> operande
+  | reg : registre -> operande
   | vide : operande.
   
   Inductive instruction : Type :=
@@ -35,8 +49,12 @@ Module mmix_en_de_code.
   Inductive operande_binary : Type :=
   | reg_binary : @Vector.t bool 8 -> operande_binary.
 
+
+
+
+  (* fonctions pour manipuler les instructions avec tests *)
   
-  Definition my_instr := instr ADD (reg 10) (reg 11) (reg 10).
+  Definition my_instr := instr ADD (reg (general_reg 10)) (reg (general_reg 11)) (reg (general_reg 10)).
   
   Check my_instr.
 
@@ -54,7 +72,7 @@ Module mmix_en_de_code.
     end.
 
   Example test_get_first_operande:
-    get_first_operande my_instr = reg 10. simpl. reflexivity. Qed.
+    get_first_operande my_instr = reg (general_reg 10). simpl. reflexivity. Qed.
   
   Definition get_second_operande (i : instruction) : operande :=
     match i with
@@ -66,6 +84,30 @@ Module mmix_en_de_code.
       | instr _  _ _ op => op
     end.
 
+    Definition test_request := "ADD 1 2 3".
+    Check test_request.
 
+    (* pour l'instant je ne fais pas encore les fonctions de parsing je fais comme si le parsing m'avais déja remplis mon 
+data type *)
 
+    (* Idée pour la suite, à l'aide de fichier on crée tout un tas d'éléments de type correspondance
+Inductive correspondance_etiquette : Type := 
+| binary_to_instruction : binary -> etiquette -> correspondance_etiquette
+| instruction_to_binary : etiquette -> binary -> correspondance_etiquette 
+     Cela permettrait donc aux fonctions de prendre une liste de ces correspondances qui seraient crée au démarage du
+     programme afin que la maintenabilité du programme soit plus aisée (parceque sinon ça serait un gros switch ?)*) 
+
+    Inductive correspondance_etiquette : Type :=
+    | opcode_to_etiquette : opcode -> etiquette -> correspondance_etiquette
+    | etiquette_to_opcode : etiquette -> opcode -> correspondance_etiquette.
+    
+    Inductive correspondance_operande : Type :=
+    | operande_to_operande_binary : operande -> operande_binary -> correspondance_operande
+    | operande_binary_to_operande : operande_binary -> operande -> correspondance_operande.
+
+    Inductive correspondance_etiquette_table : Type :=
+    | correspondance_nil : correspondance_etiquette_table
+    | correspondance_cons : etiquette -> correspondance_etiquette -> correspondance_etiquette_table.
+
+    
     
