6fb9ca1ed41c632dee479c708a21a59645b3f568
romisfrag <romisfrag@gmail.com>
2017-01-16 13:29:35 +0100

    comment are not all applied

--- mmix_en_de_code.v
+++ mmix_en_de_code.v
@@ -1,20 +1,20 @@
 Require Export Bool Sumbool.
-Require Vector.
-Export Vector.VectorNotations.
 Require Import Minus.
 Require Import Arith.
 Require Import Ascii.
 Require String.
 Open Scope string_scope.
 Open Scope list_scope.
+Require Import List.
+Import ListNotations.
 
 
 (* datatypes for the instructions *)
 
 (* XXX: Use english everywhere *)
-Inductive etiquette : Type :=
-| ADD : etiquette
-| AND : etiquette.
+Inductive tag : Type :=
+| ADD : tag
+| AND : tag.
 
 (* there is 256 register and 32 special register *)
 (* i only give two special register to test but i will not give the exhaustive list (at least for the moment) *)
@@ -22,25 +22,20 @@ Inductive special_register : Type :=
 | rB : special_register
 | rD : special_register.
 
-Inductive registre : Type :=
-| general_reg : nat -> registre
-| special_reg : special_register -> registre.
+Inductive register : Type :=
+| general_reg : nat -> register
+| special_reg : special_register -> register.
 
 Inductive operande : Type :=
 | immediate : nat -> operande
-| reg : registre -> operande
+| reg : register -> operande
 | vide : operande.
 
-Inductive instruction : Type :=
-| instr : etiquette -> operande -> operande -> operande -> instruction.
 
-(* some definition to make boolean vector manipulation easyer *)
+Record instruction :=
+  instr { firstField : tag; secondField : operande ; thirdField : operande ; fourthField : operande }.
 
-(* XXX: do not use Vectors (they are broken in Coq). Just use lists of
-   boolean, without size indexing *)
-Definition Bnil := @Vector.nil bool.
 
-Definition Bcons := @Vector.cons bool.
 
 
 (* datatypes for the binary instructions *)
@@ -49,53 +44,24 @@ Definition Bcons := @Vector.cons bool.
   need/reason to have more structure than that *)
 
 Inductive opcode : Type :=
-| opc : @Vector.t bool 8 -> opcode.
+| opc : list bool -> opcode.
 
 Inductive operande_binary : Type :=
-| reg_binary : @Vector.t bool 8 -> operande_binary.
+| reg_binary : list bool -> operande_binary.
 
 Inductive binary_instruction : Type :=
 | binary_instr : opcode -> operande_binary -> operande_binary -> operande_binary -> binary_instruction.
 
 
 
-(* fonctions pour manipuler les instructions avec tests *)
+(* Exemples d'utilisation des structures de données *)
 
 Example my_instr := instr ADD (reg (general_reg 10)) (reg (general_reg 11)) (reg (general_reg 10)).
 
 Check my_instr.
 
-(* XXX: if you are to write getters, then probably that you wanted to
-   define a [Record] instead *)
-
-Definition get_etiquette (i : instruction) : etiquette :=
-  match i with
-  | instr tag _ _ _ => tag
-  end.
-
-Example test_get_etiquette:
-  get_etiquette my_instr = ADD. simpl. reflexivity. Qed.
-
-Definition get_first_operande (i : instruction) : operande :=
-  match i with
-  | instr _ op _ _ => op
-  end.
-
-Example test_get_first_operande:
-  get_first_operande my_instr = reg (general_reg 10). simpl. reflexivity. Qed.
-
-Definition get_second_operande (i : instruction) : operande :=
-  match i with
-  | instr _  _ op _ => op
-  end.
-
-Definition get_third_operande (i : instruction) : operande :=
-  match i with
-  | instr _  _ _ op => op
-  end.
-
-Definition test_request := "ADD 1 2 3".
-Check test_request.
+Example first_field_instr := my_instr.(firstField).
+Check first_field_instr.
 
 (* pour l'instant je ne fais pas encore les fonctions de parsing je fais comme si le parsing m'avais déja remplis mon 
 data type *)
@@ -104,55 +70,46 @@ data type *)
    software gives us a list of [instruction] *)
 
 (* Idée pour la suite, à l'aide de fichier on crée tout un tas d'éléments de type correspondance
-Inductive correspondance_etiquette : Type := 
-| binary_to_instruction : binary -> etiquette -> correspondance_etiquette
-| instruction_to_binary : etiquette -> binary -> correspondance_etiquette 
+Inductive correspondance_tag : Type := 
+| binary_to_instruction : binary -> tag -> correspondance_tag
+| instruction_to_binary : tag -> binary -> correspondance_tag 
      Cela permettrait donc aux fonctions de prendre une liste de ces correspondances qui seraient crée au démarage du
      programme afin que la maintenabilité du programme soit plus aisée (parceque sinon ça serait un gros switch ?)*) 
 
-(* XXX: this is just a pair [opcode * etiquette] *)
-Inductive correspondance_etiquette : Type :=
-| opcode_to_etiquette : opcode -> etiquette -> correspondance_etiquette.
+(* XXX: this is just a pair [opcode * tag] *)
+Inductive correspondance : Type :=
+| opcode_to_tag : (opcode*tag) -> correspondance
+| operande_to_operande_binary : (operande * operande_binary) -> correspondance.
 
-Inductive correspondance_operande : Type :=
-| operande_to_operande_binary : operande -> operande_binary -> correspondance_operande.
-\
+
 (* XXX: this is just an association list, of type [list (opcode *
-   etiquette)]. But I recommend using [MSet]
+   tag)]. But I recommend using [MSet]
    [https://coq.inria.fr/library/Coq.MSets.MSets.html] instead. This
    library is hard to import, let me know if you need help. *)
 
-Inductive correspondance_etiquette_table : Type :=
-| correspondance_nil : correspondance_etiquette_table
-| correspondance_cons : correspondance_etiquette -> correspondance_etiquette_table -> correspondance_etiquette_table.
-
-Inductive correspondance_operande_table : Type :=
-| correspondance_op_nil : correspondance_operande_table
-| correspondance_op_cons : correspondance_operande -> correspondance_operande_table -> correspondance_operande_table.
-
+Inductive correspondance_table : Type :=
+| c_table : list correspondance -> correspondance_table.
 
 (* TODO :: function which take a decimal value and return the vector of bites corrsponding to it's binary representation *)
 
-Definition create_a_vector := (Bcons true 7
-                                     (Bcons true 6
-                                            (Bcons true 5
-                                                   (Bcons true 4
-                                                          (Bcons true 3 (Bcons true 2 (Bcons true 1 (Bcons true 0 Bnil)))))))).
+Definition create_a_list := [true;true;true;true;true;true;true;true].
+Definition create_a_list_bis := true :: true :: true :: true :: true :: true :: true :: true :: [].
 
-Definition my_instr_binary := binary_instr (opc create_a_vector) (reg_binary create_a_vector) (reg_binary create_a_vector)
-                                           (reg_binary create_a_vector).
 
-Definition ADD_correspondance :=  opcode_to_etiquette (opc create_a_vector) ADD.
-Definition AND_correspondance :=  opcode_to_etiquette (opc create_a_vector) AND.
+Definition my_instr_binary := binary_instr (opc create_a_list) (reg_binary create_a_list) (reg_binary create_a_list)
+                                           (reg_binary create_a_list).
+
+Definition ADD_correspondance :=  opcode_to_tag ((opc create_a_list),ADD).
+Definition AND_correspondance :=  opcode_to_tag ((opc create_a_list),AND).
 Check ADD_correspondance.
-Definition correspondance_table := correspondance_cons (ADD_correspondance) correspondance_nil.
+Definition correspondance_table_example := c_table (ADD_correspondance :: AND_correspondance :: []).
 
 
 (* Fonctions de comparaisons *)
 
 (* XXX: Check [Scheme Equality for ident1] in [https://coq.inria.fr/refman/Reference-Manual015.html] *)
 
-Definition etiquettes_equals (e1 e2 : etiquette) : bool :=
+Definition tags_equals (e1 e2 : tag) : bool :=
   match (e1,e2) with
   | (ADD,ADD) => true
   | (AND,AND) => true
@@ -161,26 +118,25 @@ Definition etiquettes_equals (e1 e2 : etiquette) : bool :=
 
 
 (* Fonctions de décodage *)
-
 (* Premiere fonction afin de décoder une étiquette *)
-
 (* XXX: use the operations (in the standard library) related to [MSet] *)
 
-Fixpoint etiquette_to_binary (t : correspondance_etiquette_table) (etiq : etiquette) : (option opcode) :=
+
+(* TODO :: need to change eveything and i need to try to make some little function to manipulate theese structures *)
+Fixpoint tag_to_binary (t : correspondance_table) (etiq : tag) : (option opcode) :=
   match t with
-  | correspondance_nil => None
-  | correspondance_cons elem suite => match elem with
-                                     | opcode_to_etiquette o e => if etiquettes_equals etiq e
+    | c_table elem => match elem with
+                                     | opcode_to_tag o e => if tags_equals etiq e
                                                                  then Some o
-                                                                 else etiquette_to_binary suite etiq
+                                                                 else tag_to_binary suite etiq
                                      end
   end.
 
 
-Fixpoint instruction_to_binary (t_etiquette : correspondance_etiquette_table)
+Fixpoint instruction_to_binary (t_tag : correspondance_tag_table)
          (t_opcode : correspondance_operande_table) (i : instruction) : binary_instruction :=
   my_instr_binary.
-match etiquette_to_binary t_etiquette (* get_etquette i *) with
+match tag_to_binary t_tag (* get_etquette i *) with
 | None => None
 | Some res => None (* La suite de la fonction *).
 end.
@@ -194,4 +150,6 @@ end.
 
 
 (* XXX: what are the theorems? *)
+
+
 
