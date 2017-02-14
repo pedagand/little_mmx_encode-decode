5d55591816755cc82d7ad677f448ee09ea77527f
romisfrag <romisfrag@gmail.com>
2017-02-14 16:09:40 +0100

    lol

--- test_binary.v
+++ test_binary.v
@@ -7,12 +7,16 @@ Fixpoint bit_n (l : list bool) : nat :=
     | a :: tl => (if a then 1 else 0) + (2 * bit_n tl)
   end.
 
-Definition testList := [ false ; false ; true ; false].
+Definition testList := [ true ; false ; true ].
 Compute bit_n testList.
 
+Definition bit_n_vrai (l : list bool) :=
+  bit_n (rev l).
+
+Definition testList' := [ true ; false ; false ].
+Compute bit_n_vrai testList'.
+
 Fixpoint n_bit (n : nat) (k : nat) : option (list bool) :=
-  if beq_nat k 0 then Some []
-  else
     match n with
       | 0 => if ltb k 1 then Some [] else None
       | S n' => match n_bit n' (div k 2) with
@@ -22,26 +26,269 @@ Fixpoint n_bit (n : nat) (k : nat) : option (list bool) :=
     end.
 
 Compute n_bit 32 6.
-Compute bit_n (match (n_bit 32 6) with Some x => x | None => [] end).
-Compute bit_n (match (n_bit 12 0) with Some x => x | None => [] end).
+Compute bit_n (match (n_bit 32 6) with Some x => x | None => [true] end).
+Compute bit_n (match (n_bit 12 55) with Some x => x | None => [true] end).
+Compute bit_n (match (n_bit 12 0) with Some x => x | None => [true] end).
 Compute bit_n [true;true;false].
 
 
+Function true_in_list (l : list bool) : bool :=
+  match l with
+    | [] => false
+    | h :: tl => if h then true else true_in_list tl
+  end.
 
-Theorem n_bit_n : forall (l : list bool) (n k : nat),
+Theorem n_bit_n : forall  (l : list bool) (n k : nat),
                     n_bit n k = Some l -> bit_n l = k.
 Proof.
-  assert (I : forall (l : list bool) (n k : nat), n_bit n k = Some l -> bit_n l = k).
+  assert (I : forall  (l : list bool) (n k : nat), n_bit n k = Some l -> bit_n l = k).
   {
-    intros l n k.
-    induction k.
-    assert (I_1 : n_bit n 0 = Some l -> bit_n l = 0).
+    induction l.
+    assert (I_1 : forall n k : nat, n_bit n k = Some [] -> bit_n [] = k).
     {
+      admit.
+    }
+    exact I_1.
+    assert (I_2 : forall n k : nat, n_bit n k = Some (a :: l) -> bit_n (a :: l) = k).
+    {
+      admit.
+    }
+    exact I_2.
+  }
+  exact I.
+Admitted.
+Theorem n_bit_n' : forall (n k : nat) (l : list bool),
+                     n_bit n k = Some l -> bit_n l = k.
+Proof.
+  assert (I : forall (n k : nat) (l : list bool), n_bit n k = Some l -> bit_n l = k).
+  {
+    induction n.
+    assert (I_1 : forall (k : nat) (l : list bool), n_bit 0 k = Some l -> bit_n l = k).
+    {
+      admit.
+    }
+    exact I_1.
+    assert (I_2 : forall (k : nat) (l : list bool), n_bit (S n) k = Some l -> bit_n l = k).
+    {
+      admit.
+    }
+    exact I_2.
+  }
+  exact I.
+Admitted.
+
+
+Theorem n_bit_n'' : forall (k n : nat) (l : list bool),
+                     n_bit n k = Some l -> bit_n l = k.
+Proof.
+  assert(I : forall (k n : nat) (l : list bool), n_bit n k = Some l -> bit_n l = k).
+  {
+    induction k.
+    assert (I_1 : forall (n : nat) (l : list bool), n_bit n 0 = Some l -> bit_n l = 0).
+    {
+      induction l.
+      assert (I_1_1 : n_bit n 0 = Some [] -> bit_n [] = 0).
+      {
+        reflexivity.
+      }
+      exact I_1_1.
+      assert (I_1_2 : n_bit n 0 = Some (a :: l) -> bit_n (a :: l) = 0).
+      {
+        induction n.
+        assert (I_1_2_1 : n_bit 0 0 = Some (a :: l) -> bit_n (a :: l) = 0).
+        {
+          simpl.
+          intros H.
+          discriminate.
+        }
+        exact I_1_2_1.
+        assert (I_1_2_2 : n_bit (S n) 0 = Some (a :: l) -> bit_n (a :: l) = 0).
+        {
+          assert (I_1_2_2_1 : forall n' l', length l' = n' -> n_bit (S n') 0 = Some (false :: l')).
+          {
+            intros n' l' H.
+            induction n'.
+            -simpl.
+             apply length_zero_iff_nil in H.
+             rewrite H.
+             reflexivity.
+            -
+          }
+          destruct a.
+
+        }
+      }
+    }
+    exact I_1.
+    assert (I_2 : forall (n : nat) (l : list bool), n_bit n (S k) = Some l -> bit_n l = S k).
+    {
+      admit.      
+    }
+    exact I_2.
+  }
+  exact I.
+
+
+
+
+
+
+Theorem n_bit_n : forall (n k : nat) (l : list bool),
+                    n_bit n k = Some l -> bit_n l = k.
+Proof.
+  assert (I : forall (n k: nat) (l : list bool), n_bit n k = Some l -> bit_n l = k).
+  {
+    (* induction n. *)
+    (* assert (I_1 : forall (k : nat) (l : list bool), n_bit 0 k = Some l -> bit_n l = k). *)
+    (* { *)
+    (*   induction k. *)
+    (*   assert (I_1_1 : forall l : list bool, n_bit 0 0 = Some l -> bit_n l = 0). *)
+    (*   { *)
+    (*     simpl. *)
+    (*     intros l H. *)
+    (*     inversion H. *)
+    (*     simpl. *)
+    (*     reflexivity. *)
+    (*   } *)
+    (*   exact I_1_1. *)
+    (*   assert (I_1_2 : forall l : list bool, n_bit 0 (S k) = Some l -> bit_n l = S k). *)
+    (*   { *)
+    (*     intros l. *)
+    (*     simpl. *)
+    (*     intros H. *)
+    (*     discriminate. *)
+    (*   } *)
+    (*   exact I_1_2. *)
+    (* }     *)
+    (* exact I_1. *)
+    (* assert (I_2 : forall (k : nat) (l : list bool), n_bit (S n) k = Some l -> bit_n l = k). *)
+    (* { *)
+    (*   intros k l. *)
+    (*   intros H. *)
+    (*   induction k. *)
+    (*   assert (I_2_1 : bit_n l = 0). *)
+    (*   { *)
+    (*     assert (I_2_1_2 : n_bit (S n) 0 = Some []). *)
+    (*     { *)
+          
+    (*     } *)
+    (*   } *)
+    (* } *)
+Admitted.
+
+
+
+
+
+
+(* fail again *)
+Theorem n_bit_n' : forall (k n : nat) (l : list bool),
+                    n_bit n k = Some l -> bit_n l = k.
+Proof.
+  assert (I : forall (k n: nat) (l : list bool), n_bit n k = Some l -> bit_n l = k).
+  {    
+    induction k.
+    assert (I_1 : forall (n : nat) (l : list bool), n_bit n 0 = Some l -> bit_n l = 0).
+    {
+      intros n l.
+      intros H.
+      induction n.
+      -simpl.
+       simpl in H.
+       inversion H.
+       reflexivity.
+      -induction l.
+       +reflexivity.
+       +destruct a.
+        {
+          admit.          
+        }
+        {
+          assert (I_1_2 : bit_n (false :: l) = 0).
+          {
+            assert (I_1_2_1 : forall l', bit_n l' = 0 -> bit_n (false :: l') = 0).
+            {
+              intros l' H'.
+              induction l'.
+              -reflexivity.              
+              -destruct a.
+               +simpl.
+                discriminate.
+               +assert (I_1_2_1_1 : forall l'', bit_n (false :: l'') = 2 * (bit_n l'')).
+                {
+                  intros l''.
+                  simpl. reflexivity.
+                }
+                rewrite I_1_2_1_1.
+                rewrite H'.
+                reflexivity.
+            }
+            rewrite I_1_2_1.
+            reflexivity.
+            
+
+            
+          }
+        }
+         
+                   
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      intros n l.
+      assert (I_1_1 : n_bit n 0 = Some l -> bit_n l = 0).
+      {
+        assert (I_1_1_2 : n_bit n 0 = Some l -> true_in_list l = false).
+        {
+          induction n.
+          assert (I_1_1_2_1 : n_bit 0 0 = Some l -> true_in_list l = false).
+          {
+            simpl.
+            intros H.
+            inversion H.
+            reflexivity.
+          }
+          exact I_1_1_2_1.
+          assert (I_1_1_2_2 : n_bit (S n) 0 = Some l -> true_in_list l = false).
+          {
+            
+          }
+        }
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+      
       assert (I_1_1 : n_bit n 0 = Some []).
       {
-        destruct n.
+        induction n.
         -reflexivity.
-        -reflexivity.
+        -assert (I_1_1_1 : n_bit (S n) 0 = Some []).
+         {
+           simpl. rewrite IHn.
+           (* ici j'abandonne cette preuve car ce cas est impossible, l'induction sur n me semble impossible 
+            sauf si je rajoute dans n_bit un test pour si k = 0 => [] alors ça marche mais on a plus des listes de bonne taille*)
+         }
       }
       rewrite I_1_1.
       intros H.
