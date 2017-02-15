4abf1fef2ff15cda6393218f77a9e59ed3d51c74
romisfrag <romisfrag@gmail.com>
2017-02-15 19:34:44 +0100

    proof almost HERE

--- test_binary.v
+++ test_binary.v
@@ -1,4 +1,4 @@
-Require Import Bool List Arith Nat.
+Require Import Bool List Arith Nat Coq.Arith.Div2.
 Import ListNotations.
 
 Fixpoint bit_n (l : list bool) : nat :=
@@ -37,13 +37,6 @@ Compute n_bit 4 (bit_n testListe'').
 Compute bit_n [true;true;false].
 
 
-Function true_in_list (l : list bool) : bool :=
-  match l with
-    | [] => false
-    | h :: tl => if h then true else true_in_list tl
-  end.
-
-
 Theorem n_bit_n : forall (l : list bool) (n k : nat),
                     n_bit n k = Some l -> bit_n l = k.
 Proof.
@@ -70,6 +63,7 @@ Proof.
     {
       intros H.
       simpl.
+      About Nat.div2_odd.
       rewrite (Nat.div2_odd k).
       simpl.
       destruct n; simpl in H.
@@ -82,3 +76,117 @@ Proof.
   }
   assumption.
 Qed.
+
+Check nil.
+
+Lemma trivialStuf : forall (n : nat), 2 * n = n + n.
+Proof.
+  intros n. simpl. Search (_ + 0 = _). rewrite Nat.add_0_r. reflexivity.
+Qed.
+
+
+
+Theorem bit_n_bit : forall (l : list bool) (n : nat),
+                      n = length l -> (n_bit n (bit_n l)) = Some l.
+Proof.
+  assert (I : forall (l : list bool) (n : nat), n = length l -> n_bit n (bit_n l) = Some l).
+  {
+    induction l.
+    assert (I_1 : forall n : nat, n = length ([] : list bool) -> n_bit n (bit_n []) = Some []).
+    {
+      simpl.
+      intros n H.
+      rewrite H.
+      reflexivity.
+    }
+    exact I_1.
+    assert (I_2 : forall n : nat, n = length (a :: l) -> n_bit n (bit_n (a :: l)) = Some (a :: l)).
+    {
+      intros n.
+      simpl.
+      destruct a.
+      assert (I_2_1 : n = length (true :: l) -> n_bit n (bit_n (true :: l)) = Some (true :: l)).
+      {
+        simpl.
+        Search (_ + 0).
+        rewrite <- plus_n_O.
+        intros H.
+        rewrite H.
+        simpl.
+        assert (I_2_1_1 : forall l' : (list bool), bit_n l' + bit_n l' = 2 * bit_n l').
+        {
+          induction l'.
+          -reflexivity.
+          -simpl.
+           rewrite <- plus_n_O.
+           rewrite <- plus_n_O.
+           reflexivity.
+        }        
+        rewrite I_2_1_1.
+        Search (_ + 1 = S _).
+        rewrite Nat.add_1_r.
+        rewrite <- even_div2.
+        -Search (Nat.div2 (2 * _)).
+         rewrite div2_double.
+         rewrite IHl.
+         +assert (I_2_1_2 : forall (n' : nat), Nat.odd (S (2 * n')) = true).
+          {
+            intros n'.
+            induction n'.
+            -reflexivity.
+            -simpl.
+             rewrite <- plus_n_Sm.
+             simpl in IHn'.
+             rewrite <- IHn'.
+             Search (Nat.odd (S (S _))).
+             rewrite Nat.odd_succ_succ. reflexivity.
+          }
+          rewrite I_2_1_2.
+          reflexivity.
+         +reflexivity.
+        -assert (I_2_1_3 : Even.even (2 * bit_n l)).
+         {
+           (* this is supposed to be trivial -_-_-_-_-_-_-_-_-_-_-_-_- *)
+           admit.
+         }
+         exact I_2_1_3.
+      }
+      exact I_2_1.
+      assert (I_2_2 : n = S (length l) -> n_bit n (bit_n l + (bit_n l + 0) + Nat.b2n false) = Some (false :: l)).
+      {
+        simpl.
+        Search (_ + 0).
+        rewrite <- plus_n_O.
+        rewrite <- plus_n_O.
+        intros H.
+        rewrite H.
+        simpl.
+        Search (2 * _ = _).
+        rewrite <- trivialStuf.
+        rewrite div2_double.
+        rewrite IHl.
+        -assert (I_2_2_1 : forall (n' : nat), Nat.odd (2 * n') = false).
+         {
+           induction n'.
+           -simpl. Search (Nat.odd 0).
+            rewrite Nat.odd_0. reflexivity.
+           -Search (2 * S _).
+           admit.
+         }
+         rewrite I_2_2_1.
+         reflexivity.
+        -reflexivity.
+      }
+      exact I_2_2.
+    }
+    exact I_2.
+  }
+  exact I.
+         
+         even_div2 
+
+       
+        
+      } 
+    }
+  }
