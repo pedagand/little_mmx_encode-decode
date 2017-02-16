1950ec8d3915caa80f17231cb19d4b45f3d3dde9
romisfrag <romisfrag@gmail.com>
2017-02-16 11:38:57 +0100

    almost final form

--- binary.v
+++ binary.v
@@ -1,239 +1,180 @@
-Require Import Bool List Arith Nat.
+Require Import Bool List Arith Nat Coq.Arith.Div2.
 Import ListNotations.
 
 
-Fixpoint bit_n_rec (l : list bool) : nat :=
+
+Fixpoint bit_n (l : list bool) : nat :=
   match l with
     | [] => 0
-    | a :: tl => (if a then 1 else 0) + (2 * bit_n_rec tl)
+    | a :: tl => 2 * bit_n tl + Nat.b2n a
   end.
 
-Definition bit_n (l : list bool) : nat :=
-  bit_n_rec (rev l).
 
-Definition testing_liste := [false;false;true;false].
-Compute bit_n testing_liste.
-
-(* this function take n as the length of the outside vector (it can fail if the lenght given is not enought to store the 
-boolean *)
-Search "beq".
-Compute ltb 0 0.
-Check beq_nat.
-Compute div 4 2.
-Check beq_nat.
-Fixpoint n_bit_rec (n : nat) (k : nat) : option (list bool) :=
-  if beq_nat k 0 then Some []
-  else 
+Fixpoint n_bit (n : nat) (k : nat) : option (list bool) :=
     match n with
-      | 0 => if ltb k 1 then Some [] else None
-      | S n' => match n_bit_rec n' (div k 2) with
+      | 0 => match k with
+             | 0 => Some []
+             | S _ => None
+             end
+      | S n' => match n_bit n' (Nat.div2 k) with
                   | None => None
-                  | Some l => Some ((beq_nat (modulo k 2) 1) :: l)
+                  | Some l => Some (Nat.odd k :: l)
                 end
     end.
 
-
-Definition n_bit (n : nat) (k : nat) : option (list bool) :=
-  match n_bit_rec n k with
-    | None => None
-    | Some x => Some(List.rev x)
-  end.
-
-Compute n_bit 10 7.
-Compute bit_n [].
-
-
-Check 2 >= 3.
-Compute 3 <= 2.
-
-(* Theorem bit_n_bit : forall (l : list bool) (n : nat), *)
-(*                       n >= length l -> (n_bit n (bit_n l)) = Some l. *)
-(* Proof. *)
-(*   assert (I : forall (l : list bool) (n : nat), n >= length l -> n_bit n (bit_n l) = Some l). *)
-(*   { *)
-(*     induction l. *)
-(*     -intros n H. *)
-     
-     
-(*   } *)
-  
-(* Theorem n_bit_n : forall (l : list bool) (n k : nat), *)
-(*                     n_bit n k = Some l -> bit_n l = k. *)
-(* Proof. *)
-(*   assert (I : forall (l : list bool) (n k : nat), n_bit n k = Some l -> bit_n l = k). *)
-(*   { *)
-(*     intros l n k. *)
-(*     induction l. *)
-(*     assert (I_1 : n_bit n k = Some [] -> bit_n [] = k). *)
-(*      { *)
-(*        induction k. *)
-(*        -intros. compute. reflexivity. *)
-(*        -assert (I_1_1 : n_bit n (S k) = Some [] -> False). *)
-(*         {           *)
-(*           destruct (n_bit n (S k)). *)
-(*           -admit. *)
-(*           -discriminate. *)
-(*         } *)
-(*         intros H. *)
-(*         compute. *)
-(*         assert (I_1_2 : ((bit_n [] = S k) -> False)). *)
-(*         { *)
-(*           compute. *)
-(*           apply Nat.neq_0_succ. *)
-(*         } *)
-(*         apply False_ind. *)
-(*         apply I_1_1. *)
-(*         rewrite H. *)
-(*         reflexivity.         *)
-(*      } *)
-(*     -exact I_1. *)
-(*     -assert (I_2 : n_bit n k = Some (a :: l) -> bit_n (a :: l) = k). *)
-(*      { *)
-(*        destruct a. *)
-(*        assert(I_2_1 : n_bit n k = Some (true :: l) -> bit_n (true :: l) = k). *)
-(*        { *)
-(*          assert (I_2_1_1 : bit_n (true :: l) = k -> bit_n l =  *)
-(*        } *)
-         
-       
-(*      } *)
-(*     exact I_2. *)
-(*   } *)
-(*   exact I. *)
-(*   (* intros l.  *) *)
-(*   (* induction l. *) *)
-(*   (* { *) *)
-(*   (*   assert (i0 : forall (n k : nat), n_bit n k = Some [] -> bit_n [] = k). *) *)
-(*   (*   { *) *)
-(*   (*     destruct k. *) *)
-(*   (*     -reflexivity. *) *)
-(*   (*     -assert (j0 : n_bit n (S k) = Some [] -> bit_n [] = S k). *) *)
-(*   (*      { *) *)
-(*   (*        induction k. *) *)
-(*   (*        -intros H. *) *)
-(*   (*         induction n. *) *)
-(*   (*         +compute in H. discriminate. *) *)
-(*   (*         +rewrite <- H in IHn. *) *)
-(*   (*          apply IHn. *) *)
-(*   (*          assert(l0 : n_bit n 1 <> n_bit (S n) 1). *) *)
-(*   (*          { *) *)
-(*   (*            induction n. *) *)
-(*   (*            -compute. intros. discriminate. *) *)
-(*   (*            -rewrite H.  *) *)
-(*   (*          } *) *)
-           
-
-(*   (*      } *) *)
-(*   (*   }     *) *)
-    
+(* first proof that we need on binary representation *)
+Theorem n_bit_n : forall (l : list bool) (n k : nat),
+                    n_bit n k = Some l -> bit_n l = k.
+Proof.
+  assert (I : forall (l : list bool) (n k : nat), n_bit n k = Some l -> bit_n l = k).
+  {
+    intros l; induction l; intros n k.
+    simpl.
+    assert (I_1 : n_bit n k = Some [] -> bit_n [] = k).
+    {
+      induction k.
+      - reflexivity.
+       (* the hypothesis is false so we will need to find how to demonstrate this *)
+      - assert (I_1_1 : n_bit n (S k) = Some [] -> bit_n [] = S k).
+       {
+         induction n.
+         - discriminate.
+         - unfold n_bit; fold n_bit.
+           destruct (n_bit n (Nat.div2 (S k))); discriminate.
+       }
+       exact I_1_1.
+    }
+    exact I_1.
+    assert (I_2 : n_bit n k = Some (a :: l) -> bit_n (a :: l) = k).
+    {
+      intros H.
+      simpl.
+      About Nat.div2_odd.
+      rewrite (Nat.div2_odd k).
+      simpl.
+      destruct n; simpl in H.
+      - destruct k; discriminate.
+      - destruct (n_bit n (Nat.div2 k)) eqn:Hl; try discriminate.
+        inversion H; subst.
+        erewrite IHl; eauto.
+    }
+    assumption.
+  }
+  assumption.
+Qed.
 
 
-      
-(*    (*  assert (k1 : n_bit n 0 = Some [] -> bit_n [] = 0). *) *)
-(*   (*   { *) *)
-(*   (*     intros H. *) *)
-(*   (*     compute. reflexivity. *) *)
-(*   (*   } *) *)
-(*   (*   exact k1. *) *)
-(*   (*   assert (k2 : bit_n [] = k). *) *)
-(*   (*   { *) *)
-(*   (*     compute. Search (_=_). *) *)
-(*   (*     apply eq_sym. *) *)
-(*   (*     rewrite k0. reflexivity. *) *)
-      
-      
-(*   (*   } *) *)
-      
-(*   (* } *) *)
-
-
-
-(* (* old version (* functions from positive to list bool and in the opsite way *) *)
-(* Fixpoint pos_to_list_acc (p: positive)(acc: list bool): list bool := *)
-(*   match p with *)
-(*   | xH => true :: acc *)
-(*   | xO p => pos_to_list_acc p (false :: acc) *)
-(*   | xI p => pos_to_list_acc p (true :: acc) *)
-(*   end. *)
-
-(* Definition pos_to_list (p: positive): list bool := pos_to_list_acc p []. *)
-
-(* Fixpoint list_to_pos_acc (l: list bool)(acc: positive): positive := *)
-(*   match l with *)
-(*   | [] => acc *)
-(*   | true :: l => list_to_pos_acc l (xI acc) *)
-(*   | false :: l => list_to_pos_acc l (xO acc) *)
-(*   end. *)
-
-(* Definition list_to_pos (l : list bool) : positive := list_to_pos_acc l xH. *)
-
-
-(* (* functions to convert list bool to N and in the opposite way *) *)
-(* Fixpoint list_bool_to_N (l: list bool): N := *)
-(*   match l with *)
-(*   | [] => N0 *)
-(*   | true :: l => Npos (list_to_pos l) *)
-(*   | false :: l => list_bool_to_N l *)
-(*   end. *)
-
-(* Definition N_to_list_bool (n: N): list bool := *)
-(*   match n with *)
-(*   | N0 => [] *)
-(*   | Npos p => pos_to_list p *)
-(*   end. *)
-
-
-(* (* The functions that you should be using outside of this module which are nat to list bool and opposite way *) *)
-(* Definition nat_to_list_bool (n : nat) : list bool := *)
-(*   N_to_list_bool (N_of_nat n). *)
-
-(* Definition list_bool_to_nat (l : list bool) : nat := *)
-(*    nat_of_N (list_bool_to_N l). *)
-
-
-(* (* TODO :: obviously i will need to make some proof about that *) *)
-
-
-
-
-(* (* some test about the functions *) *)
-(* Set Printing All. *)
-(* Compute (5)%positive. *)
-(* Unset Printing All. *)
-(* Compute (pos_to_list 5). *)
-(* Compute nat_to_list_bool 4. *)
-(* Definition my_test_list := true :: false :: true :: false :: []. *)
-(* Compute list_bool_to_nat my_test_list. *) *)
-
-
-
-
-
-(*   (* test that can bu usefull *) *)
-  
-(* Lemma some_stuff_eq : forall (t : Type), forall (x y : t), *)
-(*                         x = y -> Some x = Some y. *)
-(* Proof. *)
-(*   intros t x y H. *)
-(*   rewrite H. *)
-(*   reflexivity. *)
-(* Qed. *)
-(* Lemma some_stuff_eq_other : forall (t : Type), forall (x y : t), *)
-(*                               Some x = Some y -> x = y. *)
-(* Proof. Admitted. *)
-(* Example test_n_bit1 : forall (x : list bool), n_bit 6 10 = Some x -> bit_n x = 10. *)
-(* Proof. *)
-(*   intros x. *)
-(*   intros H. *)
-(*   compute in H. *)
-(*   assert(x =  [false; false; true;false; true; false]). *)
-(*   { *)
-(*     assert(Some x = Some [false; false; true;false; true; false] -> x = [false; false; true;false; true; false]). *)
-(*     { *)
-(*       intros H'. apply some_stuff_eq_other. rewrite H'. reflexivity. *)
-(*     } *)
-(*     rewrite H0. reflexivity. *)
-(*     rewrite H. reflexivity. *)
-(*   } *)
-(*   rewrite H0. compute. reflexivity. *)
-(* Qed. *)
+(* second proof *)
+Theorem bit_n_bit : forall (l : list bool) (n : nat),
+                      n = length l -> (n_bit n (bit_n l)) = Some l.
+Proof.
+  assert (I : forall (l : list bool) (n : nat), n = length l -> n_bit n (bit_n l) = Some l).
+  {
+    induction l.
+    assert (I_1 : forall n : nat, n = length ([] : list bool) -> n_bit n (bit_n []) = Some []).
+    {
+      simpl.
+      intros n H.
+      rewrite H.
+      reflexivity.
+    }
+    exact I_1.
+    assert (I_2 : forall n : nat, n = length (a :: l) -> n_bit n (bit_n (a :: l)) = Some (a :: l)).
+    {
+      intros n.
+      simpl.
+      destruct a.
+      assert (I_2_1 : n = length (true :: l) -> n_bit n (bit_n (true :: l)) = Some (true :: l)).
+      {
+        simpl.
+        Search (_ + 0).
+        rewrite <- plus_n_O.
+        intros H.
+        rewrite H.
+        simpl.
+        assert (I_2_1_1 : forall l' : (list bool), bit_n l' + bit_n l' = 2 * bit_n l').
+        {
+          induction l'.
+          -reflexivity.
+          -simpl.
+           rewrite <- plus_n_O.
+           rewrite <- plus_n_O.
+           reflexivity.
+        }        
+        rewrite I_2_1_1.
+        Search (_ + 1 = S _).
+        rewrite Nat.add_1_r.
+        rewrite <- even_div2.
+        -Search (Nat.div2 (2 * _)).
+         rewrite div2_double.
+         rewrite IHl.
+         +assert (I_2_1_2 : forall (n' : nat), Nat.odd (S (2 * n')) = true).
+          {
+            intros n'.
+            induction n'.
+            -reflexivity.
+            -simpl.
+             rewrite <- plus_n_Sm.
+             simpl in IHn'.
+             rewrite <- IHn'.
+             Search (Nat.odd (S (S _))).
+             rewrite Nat.odd_succ_succ. reflexivity.
+          }
+          rewrite I_2_1_2.
+          reflexivity.
+         +reflexivity.
+        -assert (I_2_1_3 : Even.even (2 * bit_n l)).
+         {
+           (* this is supposed to be trivial -_-_-_-_-_-_-_-_-_-_-_-_- *)
+           SearchAbout (2 * _) Nat.even.
+          admit. 
+         }
+         exact I_2_1_3.
+      }
+      exact I_2_1.
+      assert (I_2_2 : n = S (length l) -> n_bit n (bit_n l + (bit_n l + 0) + Nat.b2n false) = Some (false :: l)).
+      {
+        simpl.
+        Search (_ + 0).
+        rewrite <- plus_n_O.
+        rewrite <- plus_n_O.
+        intros H.
+        rewrite H.
+        simpl.
+        Search (2 * _ = _).
+        assert (I_2_2_0 : forall (n' : nat), 2 * n' = n' + n').
+        {
+          intros n'. simpl. rewrite <- plus_n_O. reflexivity.
+        }
+        rewrite <- I_2_2_0.
+        rewrite div2_double.
+        rewrite IHl.
+        -assert (I_2_2_1 : forall (n' : nat), Nat.odd (2 * n') = false).
+         {
+           induction n'.
+           -simpl. Search (Nat.odd 0).
+            rewrite Nat.odd_0. reflexivity.
+           -simpl.
+            Search (_ + 0).
+            rewrite <- plus_n_O.
+            Search (_ + _ = _ + _).
+            rewrite <- plus_Snm_nSm.
+            Search (S _ + _).
+            rewrite plus_Sn_m.
+            Search (Nat.odd (S (S _))).
+            rewrite Nat.odd_succ_succ.
+            simpl in IHn'.
+            rewrite <- plus_n_O in IHn'.
+            rewrite IHn'.
+            reflexivity.            
+         }
+         rewrite I_2_2_1.
+         reflexivity.
+        -reflexivity.
+      }
+      exact I_2_2.
+    }
+    exact I_2.
+  }
+  exact I.
+         Admitted.
